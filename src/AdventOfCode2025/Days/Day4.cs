61a943fd5741d441cc2ade5771ef4d5a613fb924
Beb <beb.dev@hotmail.com>
2026-01-14 15:55:55 -0500

    Remove dependency on external 2D grid library. Update Day 4 Solution to use a 2D array instead.

--- src/AdventOfCode2025/Days/Day4.cs
+++ src/AdventOfCode2025/Days/Day4.cs
@@ -9,15 +9,15 @@ namespace AdventOfCode2025.Days
 		{
 			int sums = 0;
 
-			Grid2D<char> grid = ParseGrid(input);
-			int rows = grid.Rows;
-			int columns = grid.Columns;
+			char[,] grid = ParseGrid(input);
+			int rows = grid.GetLength(0);
+			int columns = grid.GetLength(1);
 
 			for (int row = 0; row < rows; row++)
 			{
 				for (int col = 0; col < columns; col++)
 				{
-					if (grid.Contains('@', row, col) && CanAccessPaperRoll(grid, row, col))
+					if (CanAccessPaperRoll(grid, row, col))
 					{
 						sums++;
 					}
@@ -31,53 +31,72 @@ namespace AdventOfCode2025.Days
 		{
 			int sums = 0;
 
-			Grid2D<char> grid = ParseGrid(input);
-			int rows = grid.Rows;
-			int columns = grid.Columns;
+			char[,] grid = ParseGrid(input);
+			int rows = grid.GetLength(0);
+			int columns = grid.GetLength(1);
 
 			for (int row = 0; row < rows; row++)
 			{
 				for (int col = 0; col < columns; col++)
 				{
-					if (grid.Contains('@', row, col) && CanAccessPaperRoll(grid, row, col))
+					if (CanAccessPaperRoll(grid, row, col))
 					{
-						grid.Set('.', row, col);
+						// Remove roll, then check past rows and cols
+						grid[row, col] = '.';
+						row = Math.Max(row - 2, 0);
+						col = Math.Max(col - 1, 0);
 						sums++;
-						row = Math.Max(row-2, 0);
-						col = Math.Max(col-1, 0);
 					}
 				}
 			}
 
-			
 			return sums.ToString();
 		}
 
-		public static bool CanAccessPaperRoll(Grid2D<char> grid, int row, int column)
+		public static bool CanAccessPaperRoll(char[,] grid, int row, int col)
 		{
-			int count = 0;
+			if (!ContainsPaperRoll(grid, row, col))
+			{
+				return false;
+			}
+
+			int paperRolls = 0;
+			int maxPaperRolls = 3;
 
 			// Left, Right, Top, Down
-			if (grid.Contains('@', row-1, column)) { count++; }
-			if (grid.Contains('@', row+1, column)) { count++; }
-			if (grid.Contains('@', row, column-1)) { count++; }
-			if (grid.Contains('@', row, column+1)) { count++; }
+			if (ContainsPaperRoll(grid, row - 1, col)) { paperRolls++; }
+			if (ContainsPaperRoll(grid, row + 1, col)) { paperRolls++; }
+			if (ContainsPaperRoll(grid, row, col - 1)) { paperRolls++; }
+			if (ContainsPaperRoll(grid, row, col + 1)) { paperRolls++; }
 
 			// Top-Left, Down-Left, Top-Right, Down-Right
-			if (grid.Contains('@', row-1, column-1)) { count++; }
-			if (grid.Contains('@', row-1, column+1)) { count++; }
-			if (grid.Contains('@', row+1, column-1)) { count++; }
-			if (grid.Contains('@', row+1, column+1)) { count++; }
+			if (ContainsPaperRoll(grid, row - 1, col - 1)) { paperRolls++; }
+			if (ContainsPaperRoll(grid, row - 1, col + 1)) { paperRolls++; }
+			if (ContainsPaperRoll(grid, row + 1, col - 1)) { paperRolls++; }
+			if (ContainsPaperRoll(grid, row + 1, col + 1)) { paperRolls++; }
 
-			return count < 4;
+			return paperRolls <= maxPaperRolls;
 		}
 
-		public static Grid2D<char> ParseGrid(string[] input)
+		public static bool ContainsPaperRoll(char[,] grid, int row, int col)
+		{
+			int rows = grid.GetLength(0);
+			int columns = grid.GetLength(1);
+
+			if (row < 0 || col < 0 || row >= rows || col >= columns)
+			{
+				return false;
+			}
+
+			return grid[row, col] == '@';
+		}
+
+		public static char[,] ParseGrid(string[] input)
 		{
 			int rows = input.Length;
 			int columns = input[0].Length;
 
-			char[,] chars = new char[rows, columns];
+			char[,] grid = new char[rows, columns];
 
 			for (int i = 0; i < input.Length; i++)
 			{
@@ -85,11 +104,11 @@ namespace AdventOfCode2025.Days
 
 				for (int j = 0; j < line.Length; j++)
 				{
-					chars[i, j] = line[j];
+					grid[i, j] = line[j];
 				}
 			}
 
-			return new Grid2D<char>(chars);
+			return grid;
 		}
 	}
 }
